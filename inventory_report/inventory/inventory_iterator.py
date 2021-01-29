f75a9949c102a82d741ea2974cdfe77fe5c9551c
fogo5000 <lucaslucas2008@gmail.com>
2021-01-29 18:04:27 -0300

    req 7 in progress

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,2 +1,10 @@
+from collections.abc import Iterator
+
 class InventoryIterator (Iterator):
-    def __iter__ ()
+    def __init__ (self, data):
+        self.data = data
+
+    def __next__ (self):
+        print('ola')
+        for element in self.data:
+            yield element
