0caa2fb928a3ee441ea595b6d0451bf11a1bd5ac
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-01-29 16:39:51 -0300

    req 6 done

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,26 +1,28 @@
-# from inventory_report.reports.simple_report import SimpleReport
-# from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 
-# classes = {"simples": SimpleReport, "completo": CompleteReport}
+classes = {"simples": SimpleReport, "completo": CompleteReport}
 
 
 class Inventory:
-    def import_data(path, report_type):
+    def import_dict(path):
         with open(path, encoding="utf-8") as file:
             if path.endswith(".csv"):
                 data = [data for data in csv.DictReader(file)]
             elif path.endswith(".json"):
                 data = json.load(file)
             else:
-                xml_records = minidom.parse(file).getElementsByTagName('record')
-                # data = [{[element._get_tagName()]: element.value for element in record._get_childNodes()} for record in xml_records]
-                print(xml_records[0].tagName)
+                root = ET.parse(file).getroot()
+                data = [{el.tag: el.text for el in record} for record in root]
 
-            # return data
-            # return classes[report_type].generate(data)
+            return data
+
+    def import_data(path, report_type):
+        data = Inventory.import_dict(path)
+        return classes[report_type].generate(data)
 
 
-Inventory.import_data("inventory_report/data/inventory.xml", "simples")
+print(Inventory.import_data("inventory_report/data/inventory.xml", "simples"))
