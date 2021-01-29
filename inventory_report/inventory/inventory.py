23e090ffaff1e16c9affa4926b1216cca7f75d89
frederico-campello <fredfmc33@gmail.com>
2021-01-29 16:35:15 -0300

    Requisito 04

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,24 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory(CompleteReport, SimpleReport):
     def import_data(filepath, report_type):
-        with open(filepath) as csv_file:
-            reader = csv.DictReader(csv_file)
-            data = list(reader)
-            csv_file.close()
+        if filepath.endswith(".csv"):
+            with open(filepath) as csv_file:
+                reader = csv.DictReader(csv_file)
+                data = list(reader)
+                csv_file.close()
 
-        if report_type == "simples":
-            return SimpleReport.generate(data)
-        else:
-            return CompleteReport.generate(data)
+        if filepath.endswith(".json"):
+            with open(filepath) as json_file:
+                data = json.load(json_file)
+                json_file.close()
+
+        return (
+            SimpleReport.generate(data)
+            if report_type == "simples"
+            else CompleteReport.generate(data)
+        )
