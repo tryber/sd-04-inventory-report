34ada5ae1cf093a2ade2fcfa48217653f2004f8e
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-29 15:51:43 -0300

    Req.1 - SimpleReport.generate() - esboço

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,22 @@
+import json
+
+
+class SimpleReport:
+    def __init__(self, stock):
+        self.stock = stock
+
+    def generate(self):
+        # Leitura do arquivo
+        with open(self.stock) as file:
+            result = json.load(file)
+        print(result)
+
+        # compara a chave data_de_fabricacao de todos os objetos para encontrar a mais antiga
+        print("Data de fabricação mais antiga: YYYY-MM-DD")
+
+        # compara a chave data_de_validade de todos os objetos para encontrar a mais próxima, que ainda não venceu
+        print("Data de validade mais próxima: YYYY-MM-DD")
+
+        # Utilizar a chave nome_da_empresa para contar a quantidade de produtos da empresa
+        # Depois comparar esses valores para ver quem tem o maior número de produtos estocados.
+        print("Empresa com maior quantidade de produtos estocados: NOME DA EMPRESA")
