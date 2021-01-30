d15e38881813ad4c31cbe071d16979d348438cfc
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-29 21:16:41 -0300

    Req. 1 - simple_report passando localmente

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,4 @@
-import json
+import datetime
 from collections import Counter
 
 """
@@ -24,23 +24,30 @@ class SimpleReport:
     def generate(self):
         stock = self
 
-        # compara a chave data_de_fabricacao de todos os objetos para encontrar a mais antiga
+        today = datetime.date.today()
+
         manufacture_date = []
-        validation_date = []
+        validity_date = []
         company_name = []
         for product in stock:
             manufacture_date.append(product['data_de_fabricacao'])
-            validation_date.append(product['data_de_validade'])
+            validity_date.append(product['data_de_validade'])
             company_name.append(product['nome_da_empresa'])
 
+        validity_prox_Date = []
+        for validity in validity_date:
+            proxValidity = datetime.date.fromisoformat(validity)
+            if proxValidity > today:
+                validity_prox_Date.append(validity)
+
         company_great_than_qtd = Counter(company_name)
-        print('manufacture_date:', min(manufacture_date))
-        print('validation_date:', validation_date)
-        print('company_great_than_qtd:', company_great_than_qtd)
+        print('\ncompany_great_than_qtd:', company_great_than_qtd)
+        print('company_great_than_qtd2:', max(company_great_than_qtd))
+
         simple_report = (
             f"Data de fabricação mais antiga: {min(manufacture_date)}\n"
-            f"Data de validade mais próxima: {max(validation_date)}\n"
-            f"Empresa com maior quantidade de produtos estocados: {company_great_than_qtd[0]}"
+            f"Data de validade mais próxima: {min(validity_prox_Date)}\n"
+            f"Empresa com maior quantidade de produtos estocados: {max(company_great_than_qtd)}\n"
         )
 
         return simple_report
