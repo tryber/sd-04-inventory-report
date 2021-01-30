9cc3851dea25981efca5d6c8a93ee3a33500f6ea
Josias Viveiro <josiasviveiro@gmail.com>
2021-01-29 21:31:25 -0300

    github-actions - Erros - 0

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,21 +1,6 @@
 import datetime
 from collections import Counter
 
-"""
-with open('../data/inventory.json', 'r') as file:
-    content = file.read()
-    data = json.loads(content)
-
-
-company_name = []
-
-for test in data:
-    company_name.append(test['nome_da_empresa'])
-
-print(company_name)
-company_great_than_qtd = Counter(company_name)
-print(company_great_than_qtd)
-"""
 
 class SimpleReport:
     def __init__(self, stock):
@@ -41,13 +26,12 @@ class SimpleReport:
                 validity_prox_Date.append(validity)
 
         company_great_than_qtd = Counter(company_name)
-        print('\ncompany_great_than_qtd:', company_great_than_qtd)
-        print('company_great_than_qtd2:', max(company_great_than_qtd))
 
         simple_report = (
             f"Data de fabricação mais antiga: {min(manufacture_date)}\n"
             f"Data de validade mais próxima: {min(validity_prox_Date)}\n"
-            f"Empresa com maior quantidade de produtos estocados: {max(company_great_than_qtd)}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            f"{max(company_great_than_qtd)}\n"
         )
 
         return simple_report
