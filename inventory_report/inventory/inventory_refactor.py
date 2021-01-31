a3219543b85d92f5c703534d825677647b07d64f
Filipe de Castro <filipeyoga@gmail.com>
2021-01-31 10:38:16 -0300

    req7

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,12 +1,7 @@
-from inventory_report.inventory.inventory_iterator import InventoryIterator
+from ..inventory.inventory_iterator import InventoryIterator
 from ..reports.complete_report import CompleteReport
 from ..reports.simple_report import SimpleReport
-import xml.etree.ElementTree as ET
 from collections.abc import Iterable
-from ..importer import Importer
-from inventory_report.reports.simple_report import SimpleReport
-from inventory_report.reports.complete_report import CompleteReport
-from inventory_report.inventory.inventory_iterator import InventoryIterator
 
 Report = {"simples": SimpleReport, "completo": CompleteReport}
 
@@ -18,7 +13,7 @@ class InventoryRefactor(Iterable):
 
     def import_data(self, path, report_type):
         self.data += self.importer.import_data(path)
-        return Report[report_type].generate(self.data)
+        return Report[report_type].generate(self.data.copy())
 
     def __iter__(self):
         return InventoryIterator(self.data)
