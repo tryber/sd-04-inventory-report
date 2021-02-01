0ca168b06d403c8c77879d5b88be7b777edfd572
frederico-campello <fredfmc33@gmail.com>
2021-02-01 11:19:30 -0300

    Complexidade do metodo import_data

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,30 +5,47 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
+class CSVImporter:
+    def import_csv(filepath):
+        with open(filepath) as csv_file:
+            reader = csv.DictReader(csv_file)
+            return list(reader)
+            csv_file.close()
+
+
+class JSONImporter:
+    def import_json(filepath):
+        with open(filepath) as json_file:
+            return json.load(json_file)
+            json_file.close()
+
+
+class XMLImporter:
+    def import_xml(filepath):
+        tree = ElementTree.parse(filepath)
+        root = tree.getroot()
+        data = []
+
+        for product in root:
+            obj = {}
+            for item in product:
+                obj[item.tag] = item.text
+                if obj not in data:
+                    data.append(obj)
+
+        return data
+
+
 class Inventory(CompleteReport, SimpleReport):
     def import_data(filepath, report_type):
         if filepath.endswith(".csv"):
-            with open(filepath) as csv_file:
-                reader = csv.DictReader(csv_file)
-                data = list(reader)
-                csv_file.close()
+            data = CSVImporter.import_csv(filepath)
 
         if filepath.endswith(".json"):
-            with open(filepath) as json_file:
-                data = json.load(json_file)
-                json_file.close()
+            data = JSONImporter.import_json(filepath)
 
         if filepath.endswith(".xml"):
-            tree = ElementTree.parse(filepath)
-            root = tree.getroot()
-            data = []
-
-            for product in root:
-                obj = {}
-                for item in product:
-                    obj[item.tag] = item.text
-                    if obj not in data:
-                        data.append(obj)
+            data = XMLImporter.import_xml(filepath)
 
         return (
             SimpleReport.generate(data)
