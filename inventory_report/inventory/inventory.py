30f7b9c5ae8c7bb090aadb20c26535e2de6cb741
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-02 13:50:16 -0300

    completed requirement 2

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,8 +5,8 @@ from inventory_report.reports.simple_report import SimpleReport
 
 class Inventory:
     @classmethod
-    def import_data(cls, csv_file, report_type):
-        with open(csv_file) as file:
+    def import_data(cls, imported_file, report_type):
+        with open(imported_file) as file:
             file_reader = csv.reader(file, delimiter=",")
             header, *data = file_reader
             file_result = [
@@ -17,4 +17,4 @@ class Inventory:
             print(simple_report_result)
 
 
-Inventory.import_data("inventory_report/data/inventory.csv", "simples")
+# Inventory.import_data("inventory_report/data/inventory.csv", "simples")
