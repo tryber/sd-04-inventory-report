4ed410d4ce08e909fd4dd09d61d18359c244b8d8
deboracosilveira <deboracosilveira@gmail.com>
2021-02-03 10:15:33 -0300

    Req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,12 +1,17 @@
 import csv
+import json
 from ..reports.simple_report import SimpleReport
 from ..reports.complete_report import CompleteReport
 
 
 class Inventory:
     def import_data(path, report_type):
-        with open(path) as file:
-            data = list(csv.DictReader(file))
+        if path.endswith(".csv"):
+            with open(path) as file:
+                data = list(csv.DictReader(file))
+        if path.endswith(".json"):
+            with open(path) as file:
+                data = json.load(file)
 
         if report_type == "simples":
             return SimpleReport.generate(data)
