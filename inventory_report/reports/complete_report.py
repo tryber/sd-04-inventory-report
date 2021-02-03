57900f4a0341784c7c3ffd2fa35d6a33e9741ed9
juniomelos <juniomelos@gmail.com>
2021-02-03 20:13:08 -0300

    Tests - complete_report.py - locally ok

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,58 @@
+import collections
+from inventory_report.reports.simple_report import SimpleReport
+
+mock = [
+    {
+        "id": 1,
+        "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "sodium ferric gluconate complex",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2020-05-31",
+        "data_de_validade": "2023-01-17",
+        "numero_de_serie": "SE95 2662 8860 5529 8299 2861",
+        "instrucoes_de_armazenamento": "duis bibendum morbi",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Dexamethasone Sodium Phosphate",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2019-09-13",
+        "data_de_validade": "2023-02-13",
+        "numero_de_serie": "BA52 2034 8595 7904 7131",
+        "instrucoes_de_armazenamento": "morbi quis tortor id",
+    },
+    {
+        "id": 4,
+        "nome_do_produto": "Uricum acidum, Benzoicum acidum",
+        "nome_da_empresa": "Newton Laboratories",
+        "data_de_fabricacao": "2019-11-08",
+        "data_de_validade": "2019-11-25",
+        "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
+        "instrucoes_de_armazenamento": "velit eu est congue elementum",
+    },
+]
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, data):
+        res_simple_report = super().generate(mock)
+        quantity_products = collections.Counter(
+            [stock["nome_da_empresa"] for stock in mock]
+        )
+
+        stocked_products = "\nProdutos estocados por empresa: \n"
+
+        for company in quantity_products:
+            stocked_products += f"- {company}: {quantity_products[company]}\n"
+            response = res_simple_report + stocked_products
+
+        return response
