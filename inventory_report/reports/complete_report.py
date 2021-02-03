9b49bad49e21ed4dab6040f7fbc8c8bc153cd86b
Tiago-ers <trsilva284@gmail.com>
2021-02-03 10:42:36 -0300

    req. 2

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,10 +1,25 @@
+from collections import Counter
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    def __init__(self, stock):
-        self.stock = stock
+    def generate(data):        
 
-    # def generate(self):
-    #     return SimpleReport.generate(self)
-    #      SimpleReport.company_name
+        company_name = []
+        for product in data:
+            company_name.append(product['nome_da_empresa'])
+        
+        company_name_qtd = Counter(company_name).items()
+        message = []
+    
+        for k, v in dict(company_name_qtd).items():
+            message.append(f"- {k}: {v}")
+        
+        format_message = '\n'.join(message)
+       
+        complete_report = (
+            f"{SimpleReport.generate(data)}\n"
+            f"Produtos estocados por empresa: \n"
+            f"{format_message}\n"
+        )
+        return complete_report
