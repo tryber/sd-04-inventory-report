5b504b1a10864c534d8a149112b90cb2ea471a42
Tiago-ers <trsilva284@gmail.com>
2021-02-04 14:11:19 -0300

    formatação code-2

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -6,7 +6,8 @@ class CsvImporter(Importer):
     def import_data(path_file):
         if path_file.endswith('.csv'):
             with open(path_file) as path_csv:
-                reader_csv = csv.DictReader(path_csv, delimiter=',', quotechar='"')
+                reader_csv = \
+                    csv.DictReader(path_csv, delimiter=',', quotechar='"')
                 return list(reader_csv)
         else:
             return ValueError('Arquivo inválido')
