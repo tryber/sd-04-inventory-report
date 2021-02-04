e9d7632d7da729e91d2249caef659acacba5d3f1
Mateus-Moreira <ashuraw@hotmail.com>
2021-02-03 22:56:52 -0300

    req 5

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,9 +1,18 @@
 from inventory_report.importer.importer import Importer
-from inventory_report.inventory.inventory import Inventory
+from xml.etree.ElementTree import parse
 
 
 class XmlImporter(Importer):
     def import_data(path):
         if not path.endswith(".xml"):
             raise ValueError("Arquivo inválido")
-        return Inventory.import_dict(path)
+        with open(path) as file:
+            data = []
+            root = parse(file).getroot()
+            for i in root:
+                item = {}
+                for pars in i:
+                    item[pars.tag] = pars.text
+                data.append(item)
+            return data
+            
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,6 +3,7 @@ from inventory_report.reports.complete_report import CompleteReport
 
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
@@ -12,6 +13,8 @@ class Inventory:
             report = CsvImporter.import_data(path)
         if path.endswith(".json"):
             report = JsonImporter.import_data(path)
+        if path.endswith(".xml"):
+            report = XmlImporter.import_data(path)
 
         if type == "simples":
             return SimpleReport.generate(report)
