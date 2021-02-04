6e371676ee7d0c0dab5e246a723b5525c6d4136d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-04 15:39:59 -0300

    completed requirement 3

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,14 @@
 import csv
-
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+    # def __init__(self, report_result)
+
     @classmethod
     def import_data(cls, imported_file, report_type):
+        cls.report_result = ""
         with open(imported_file) as file:
             file_reader = csv.reader(file, delimiter=",")
             header, *data = file_reader
@@ -13,8 +16,14 @@ class Inventory:
                 {header: data for header, data in zip(header, data)}
                 for data in data
             ]
-            simple_report_result = SimpleReport.generate(file_result)
-            print(simple_report_result)
+            if report_type == "simples":
+                cls.report_result = SimpleReport.generate(file_result)
+            elif report_type == "completo":
+                cls.report_result = CompleteReport.generate(file_result)
+            else:
+                cls.report_result = "Tipo de relatório inválido"
+            file.close()
+        return cls.report_result
 
 
-# Inventory.import_data("inventory_report/data/inventory.csv", "simples")
+# Inventory.import_data("inventory_report/data/inventory.csv", "completo")
