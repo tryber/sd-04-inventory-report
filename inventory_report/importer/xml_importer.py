803bdf9db6dde664ac1c228c90dc701f830a00ec
Josias Viveiro <josiasviveiro@gmail.com>
2021-02-05 03:03:10 -0300

    Req.2 - XmlImporter

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,11 +1,14 @@
 import xmltodict
+import json
 from inventory_report.importer.importer import Importer
 
 
 class XmlImporter(Importer):
     def import_data(path_file):
-        if path_file.endswith('.json'):
-            path_xml = open(path_file)
-            data_xml = path_xml.read()
-            data_file = xmltodict.parse(data_xml)
+        if path_file.endswith('.xml'):
+            with open(path_file) as path_xml:
+                data_xml = path_xml.read()
+                xml_dict = xmltodict.parse(data_xml)
+            data_json = json.dumps(xml_dict["dataset"]["record"])
+            data_file = json.loads(data_json)
             return data_file
