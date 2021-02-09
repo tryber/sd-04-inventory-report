2edbe450be51080f6875798a8ec23d90e10f17cd
Richard Wellerson <richard.martins@sambatech.com.br>
2021-02-09 18:23:40 -0300

    Changes about flake8

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,4 +1,3 @@
-import sys
 from reports.simple_report import SimpleReport
 
 
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -11,11 +11,19 @@ class SimpleReport:
 
     @classmethod
     def generate(self, list_dict):
-        oldest_date = min(list_dict, key=itemgetter("data_de_fabricacao"))["data_de_fabricacao"]
+        oldest_date = min(list_dict, key=itemgetter("data_de_fabricacao"))[
+            "data_de_fabricacao"
+        ]
         not_expired = filter(self.greater_than_today, list_dict)
-        nearest_date = min(not_expired, key=itemgetter("data_de_validade"))["data_de_validade"]
-        counter_employees = Counter(product['nome_da_empresa'] for product in list_dict)
-        most_product_employee = max(counter_employees, key=counter_employees.get)
+        nearest_date = min(not_expired, key=itemgetter("data_de_validade"))[
+            "data_de_validade"
+        ]
+        counter_employees = Counter(
+            product["nome_da_empresa"] for product in list_dict
+        )
+        most_product_employee = max(
+            counter_employees, key=counter_employees.get
+        )
 
         report = f"Data de fabricação mais antiga: {oldest_date}\n"
         report += f"Data de validade mais próxima: {nearest_date}\n"
