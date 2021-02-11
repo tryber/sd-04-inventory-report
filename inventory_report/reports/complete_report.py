30057bd2485115d8e88655fe019059a6f7d31735
danilossousa <danilosantossousa@gmail.com>
2021-02-11 08:56:11 -0300

    v8.0.9

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,40 +1,59 @@
-from datetime import datetime
-from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+teste = [
+    {
+        "id": 1,
+        "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "sodium ferric gluconate complex",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2020-05-31",
+        "data_de_validade": "2023-01-17",
+        "numero_de_serie": "SE95 2662 8860 5529 8299 2861",
+        "instrucoes_de_armazenamento": "duis bibendum morbi",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Dexamethasone Sodium Phosphate",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2019-09-13",
+        "data_de_validade": "2023-02-13",
+        "numero_de_serie": "BA52 2034 8595 7904 7131",
+        "instrucoes_de_armazenamento": "morbi quis tortor id",
+    },
+    {
+        "id": 4,
+        "nome_do_produto": "Uricum acidum, Benzoicum acidum",
+        "nome_da_empresa": "Newton Laboratories",
+        "data_de_fabricacao": "2019-11-08",
+        "data_de_validade": "2019-11-25",
+        "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
+        "instrucoes_de_armazenamento": "velit eu est congue elementum",
+    },
+]
 
 
-class CompleteReport:
-    def __init__(self):
-        return self
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, prod):
+        previousResult = super().generate(prod)
+        companies = super().company_separator(cls, prod)
+        company_str = [
+            f"- {company}: {quantity}"
+            for company, quantity in companies.items()
+        ]
+        separator = "\n"
+        return f"""{previousResult}
+Produtos Estocados por empresa: \n{separator.join(company_str)}
+"""
 
-    def generate(data):
-        today = datetime.today()
-        oldest_mfg_date = min([
-            datetime.strptime(product["data_de_fabricacao"], "%Y-%m-%d").date()
-            for product in data
-        ])
-        future_dates = []
-        for product in data:
-            d = datetime.strptime(product["data_de_validade"], "%Y-%m-%d")
-            if d > today:
-                future_dates.append(d.date())
-        closest_exp_date = min([
-            product
-            for product in future_dates
-        ])
-        all_companys = Counter([
-            product["nome_da_empresa"] for product in data
-        ])
-        company_name = all_companys.most_common(1)[0]
 
-        result = ""
-        for company in all_companys:
-            result = result + f"- {company}: {all_companys[company]}\n"
-
-        return(
-            f"Data de fabricação mais antiga: {oldest_mfg_date}\n" +
-            f"Data de validade mais próxima: {closest_exp_date}\n" +
-            f"Empresa com maior quantidade de produtos estocados:"
-            f" {company_name[0]}\n\n" +
-            "Produtos estocados por empresa: \n" +
-            f"{result}"
-        )
+if __name__ == "__main__":
+    print(CompleteReport.generate(teste))
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,33 +1,63 @@
 from datetime import datetime
-from collections import Counter
+import collections
+
+teste = [
+    {
+        "id": 1,
+        "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "sodium ferric gluconate complex",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2020-05-31",
+        "data_de_validade": "2023-01-17",
+        "numero_de_serie": "SE95 2662 8860 5529 8299 2861",
+        "instrucoes_de_armazenamento": "duis bibendum morbi",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Dexamethasone Sodium Phosphate",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2019-09-13",
+        "data_de_validade": "2023-02-13",
+        "numero_de_serie": "BA52 2034 8595 7904 7131",
+        "instrucoes_de_armazenamento": "morbi quis tortor id",
+    },
+    {
+        "id": 4,
+        "nome_do_produto": "Uricum acidum, Benzoicum acidum",
+        "nome_da_empresa": "Newton Laboratories",
+        "data_de_fabricacao": "2019-11-08",
+        "data_de_validade": "2019-11-25",
+        "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
+        "instrucoes_de_armazenamento": "velit eu est congue elementum",
+    },
+]
 
 
 class SimpleReport:
-    def __init__(self):
-        return self
+    def company_separator(self, myList):
+        prod = [elem["nome_da_empresa"] for elem in myList]
+        companies = collections.Counter(prod)
+        return companies
 
-    def generate(data):
+    @classmethod
+    def generate(cls, prod):
         today = datetime.today()
-        oldest_mfg_date = min([
-            datetime.strptime(product["data_de_fabricacao"], "%Y-%m-%d").date()
-            for product in data
-        ])
-        future_dates = []
-        for product in data:
-            d = datetime.strptime(product["data_de_validade"], "%Y-%m-%d")
-            if d > today:
-                future_dates.append(d.date())
-        closest_exp_date = min([
-            product
-            for product in future_dates
-        ])
-        company_name = Counter([
-            product["nome_da_empresa"] for product in data
-        ]).most_common(1)[0]
-
-        return(
-            f"Data de fabricação mais antiga: {oldest_mfg_date}\n" +
-            f"Data de validade mais próxima: {closest_exp_date}\n" +
-            "Empresa com maior quantidade de produtos estocados:"
-            f" {company_name[0]}\n"
-            )
+        manufactured_list = [elem["data_de_fabricacao"] for elem in prod]
+        valid_list = [
+            elem["data_de_validade"]
+            for elem in prod
+            if today < datetime.strptime(elem["data_de_validade"], "%Y-%m-%d")
+        ]
+        company_stock = cls.company_separator(cls, prod).most_common(1)[0][0]
+        return f"""Data de fabricação mais antiga: {min(manufactured_list)}
+Data de validade mais próxima: {min(valid_list)}
+Empresa com maior quantidade de produtos estocados: {company_stock}
+"""
