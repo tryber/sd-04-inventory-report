7bbf60df02ea10f469851d1e196a0e14308123d1
Tiago-ers <trsilva284@gmail.com>
2021-02-11 12:52:24 -0300

    Req. 7 passando localmente

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,21 +1,21 @@
 from collections.abc import Iterable
-from inventory_iterator import InventoryIterator
+from inventory_report.inventory.inventory_iterator import InventoryIterator
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
-    def __init__(self, path):
-        self.data_inventory = []
-        self.path = path
+    def __init__(self, importer):
+        self.data = []
+        self.importer = importer
 
     def __iter__(self):
-        return InventoryIterator(self.data_inventory)
+        return InventoryIterator(self.data)
 
     def import_data(self, pathfile, mode_report):
-        self.data_inventory += self.path.import_data(pathfile)
+        self.data += self.importer.import_data(pathfile)
 
         if mode_report == 'simples':
-            return SimpleReport.generate(self.data_inventory)
+            return SimpleReport.generate(self.data)
         else:
-            return CompleteReport.generate(self.data_inventory)
+            return CompleteReport.generate(self.data)
