e4071180451d5edbb356c78318539906e56120b8
Victor L <victor.lustosacp@gmail.com>
2021-03-03 16:17:58 -0300

    requisito 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+    def __init__(self):
+        self = self
+
+    @classmethod
+    def generate(cls, lista):
+        data_e_hora_atuais = datetime.now()
+        date_time = data_e_hora_atuais.strftime("%Y/%m/%d")
+        data_mais_antiga = min([x["data_de_fabricacao"] for x in lista])
+        data_validade_proxima = min(
+            [x["data_de_validade"] for x in lista if x["data_de_validade"] > date_time])
+        estoque = max(
+            Counter([x["nome_da_empresa"] for x in lista])
+        )
+        print(estoque)
+        return (
+            f"Data de fabricação mais antiga: {data_mais_antiga}\n"
+            f"Data de validade mais próxima: {data_validade_proxima}\n"
+            f"Empresa com maior quantidade de produtos estocados: {estoque}\n"
+        )
---
+++ inventory_report/reports/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+        # for item in lista:
+        #     data_fabricacao.append(
+        #         datetime.datetime(item["data_de_fabricacao"], "%y-%m-%d")
+        #     )
+        #     data_validade.append(
+        #         datetime.datetime.strptime(item["data_de_validade"], "%y-%m-%d")
+        #     )
+        #     empresa.append(item["nome_da_empresa"])
