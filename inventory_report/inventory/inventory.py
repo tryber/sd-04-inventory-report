b3c0f712bdf1b3b23241174d0b53cb00640fca7c
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-11 17:53:48 -0300

    tentando 3 e 4

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,32 @@
+import csv
+import json
+from reports import simple_report, complete_report
+
+
+class Inventory:
+    def simple_complete(self, data, tipe):
+        if tipe == 'simples':
+            return simple_report.generate(data)
+        else:
+            return complete_report.generate(data)
+
+    def import_data(self, filepath, tipe):
+
+        if filepath.endswith('.csv'):
+            with open(filepath) as file:
+                data = [csv.DictReader(file, delimiter=",")]
+                simple_report(data, tipe)
+
+        elif filepath.endswith('.json'):
+            with open(filepath) as file:
+                data = file.read()
+                data_json = json.loads(data)
+                simple_report(data_json, tipe)
+
+        elif filepath.endswith('.xml'):
+            with open(filepath) as file:
+                data = file.read()
+                simple_report(data, tipe)
+
+        else:
+            raise ValueError("Formato invalido")
