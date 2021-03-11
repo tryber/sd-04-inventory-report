e836ef902604c408ad7db668d0b3e5d6387c63ab
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-11 19:12:13 -0300

    n sei o que ta acontecendo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,32 +1,52 @@
 import csv
 import json
-from reports import simple_report, complete_report
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+    # fiz esse metodo pq minha intencao eh que ele se repita nos
+    # tres requisitos pra cada tipo de arquivo
     def simple_complete(self, data, tipe):
         if tipe == 'simples':
-            return simple_report.generate(data)
+            return SimpleReport.generate(data)
         else:
-            return complete_report.generate(data)
+            return CompleteReport.generate(data)
 
     def import_data(self, filepath, tipe):
 
         if filepath.endswith('.csv'):
             with open(filepath) as file:
+                print(f"filepath {filepath}")
+                print(f"tipe {tipe}")
                 data = [csv.DictReader(file, delimiter=",")]
+                print(f"data {data}")
                 self.simple_complete(data, tipe)
 
-        elif filepath.endswith('.json'):
-            with open(filepath) as file:
-                data = file.read()
-                data_json = json.loads(data)
-                self.simple_complete(data_json, tipe)
+        # elif filepath.endswith('.json'):
+        #     with open(filepath) as file:
+        #         data = file.read()
+        #         data_json = json.loads(data)
+        #         self.simple_complete(data_json, tipe)
 
-        elif filepath.endswith('.xml'):
-            with open(filepath) as file:
-                data = file.read()
-                simple_report(data, tipe)
+        # elif filepath.endswith('.xml'):
+        #     with open(filepath) as file:
+        #         data = file.read()
+        #         simple_report(data, tipe)
 
         else:
             raise ValueError("Formato invalido")
+
+
+arg1 = 'inventory_report/data/inventory.csv'
+arg2 = 'simples'
+
+# n sei como faco pra testar essa coisa, achei q assim iria mas ele acusa erro
+# dizendo que o segundo parametro n esta sendo passado
+
+
+def teste(a, b):
+    Inventory.import_data(a, b)
+
+
+print(teste(arg1, arg2))
