3d4e207d7eadd7e3fab90e43b523e6f486129755
leandromarten1 <leandromarten1@gmail.com>
2021-03-16 15:45:43 -0300

    pull Leo - req2

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,12 +6,12 @@ class CompleteReport(SimpleReport):
     @classmethod
     def generate(cls, list_dict):
         simple_report = super().generate(list_dict)
-        qtd_products = Counter([index["nome_da_empresa"] for index in list_dict])
+        qtd_products = Counter(
+            [index["nome_da_empresa"] for index in list_dict]
+        )
         product_stocked = "Produtos estocados por empresa: \n"
-        
+
         for key, value in qtd_products.items():
             product_stocked += f"- {key}: {value}\n"
 
-        return (
-            f"{simple_report}\n{product_stocked}"
-        )
+        return f"{simple_report}\n{product_stocked}"
