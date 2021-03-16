9520651bd92d5b776c1ad98063f3a607d190822f
leandromarten1 <leandromarten1@gmail.com>
2021-03-16 15:16:35 -0300

    flake8

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -6,12 +6,20 @@ class SimpleReport:
     @classmethod
     def generate(self, list_dict):
         today = date.today().strftime("%Y-%m-%d")
-        old_fab_date = min([old['data_de_fabricacao'] for old in list_dict])
-        new_fab_date = min([x['data_de_validade'] for x in list_dict if x['data_de_validade'] > today])
-        company = max(Counter([index["nome_da_empresa"] for index in list_dict]))
+        old_fab_date = min([old["data_de_fabricacao"] for old in list_dict])
+        new_fab_date = min(
+            [
+                x["data_de_validade"]
+                for x in list_dict
+                if x["data_de_validade"] > today
+            ]
+        )
+        company = max(
+            Counter([index["nome_da_empresa"] for index in list_dict])
+        )
 
         return (
             f"Data de fabricação mais antiga: {old_fab_date}\n"
             f"Data de validade mais próxima: {new_fab_date}\n"
             f"Empresa com maior quantidade de produtos estocados: {company}\n"
-            )
+        )
