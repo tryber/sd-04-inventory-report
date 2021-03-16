c72bb053c95e8590905ce78ccd15c68a9dc60284
leandromarten1 <leandromarten1@gmail.com>
2021-03-16 15:52:24 -0300

    Requisitos 3 e 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -8,12 +10,20 @@ class Inventory:
         report = []
         if file_path.endswith(".csv"):
             report = cls.reader_csv(file_path)
+        elif file_path.endswith('.json'):
+            report = cls.reader_json(file_path)
         if type == "simples":
             return SimpleReport.generate(report)
         elif type == "completo":
-            return 'arquivo Compelto'
+            return CompleteReport.generate(report)
+
     @classmethod
     def reader_csv(cls, file_path):
         with open(file_path) as file:
             csv_file = csv.DictReader(file, delimiter=",")
             return [row for row in csv_file]
+
+    @classmethod
+    def reader_json(cls, file_path):
+        with open(file_path) as file:
+            return json.load(file)
