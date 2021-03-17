ed8bac66ad845e809c66071e11663d2e53bd3615
henrique <hbaetaleite@gmail.com>
2021-03-17 10:03:44 -0300

    tentando menu, desde ontem trabalhando nele, mas ainda nao passa local

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,9 +1,24 @@
-# from inventory_report.inventory.inventory_refactor import InventoryRefactor
-# from inventory_report.importer.csv_importer import CsvImporter
-# from inventory_report.importer.json_importer import JsonImporter
-# from inventory_report.importer.xml_importer import XmlImporter
-# import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
 
 
-# def main():
-#     pass
+def main():
+    try:
+        report_type, path = sys.argv
+
+    except ValueError:
+        print("Verifique os argumentos", file=sys.stderr)
+        return
+    if path.endswith(".csv"):
+        importer = CsvImporter
+    elif path.endswith(".json"):
+        importer = JsonImporter
+    else:
+        importer = XmlImporter
+
+    instance = InventoryRefactor(importer)
+    results = instance.import_data((path, report_type))
+    print(results, end="")
