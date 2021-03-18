b7f4f631679a929593793f47c738238fa6c5e2f3
danielPantalena <pantalenadaniel@gmail.com>
2021-03-18 10:27:57 -0300

    Create CsvImporter

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,10 @@
+from .importer import Importer
+from ..inventory.inventory import Inventory
+
+
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if not path.endswith("csv"):
+            raise ValueError("Arquivo inválido")
+        return Inventory.import_data(path)
