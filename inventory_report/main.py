93c407456877897b322320aff100f7151b82cdcd
danielPantalena <pantalenadaniel@gmail.com>
2021-03-18 10:58:06 -0300

    Refactor

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
     pass
+    try:
+        zero, path, r_type = sys.argv
+    except ValueError:
+        print('Verifique os argumentos', file=sys.stderr)
+        return
+    if path.endswith('csv'):
+        importer = CsvImporter
+    elif path.endswith('json'):
+        importer = JsonImporter
+    elif path.endswith('xml'):
+        importer = XmlImporter
+    instance = InventoryRefactor(importer)
+    result = instance.import_data(path, r_type)
+    print(result, end="")
