01fbafb25d28632bd865555f0797f00eefd5c489
danielPantalena <pantalenadaniel@gmail.com>
2021-03-18 07:18:19 -0300

    Format

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from collections import Counter
+from functools import reduce
 
 dataReport = [
     {
@@ -21,36 +21,40 @@ def transform_string_to_date(string_param):
 class SimpleReport:
     @classmethod
     def generate(cls, dataReport):
+        def get_oldest_fab_date(products_list):
+            products_list.sort(
+                key=lambda product: transform_string_to_date(
+                    product["data_de_fabricacao"]
+                )
+            )
+            return products_list[0]["data_de_fabricacao"]
+
+        def get_closest_validity(products_list):
+            today = datetime.today()
+            next_validity_dates = [
+                product["data_de_validade"]
+                for product in products_list
+                if today
+                < transform_string_to_date(product["data_de_validade"])
+            ]
+            return min(next_validity_dates)
+
+        def get_company_with_more_products(products_list):
+            companies_dict = {}
+            for product in products_list:
+                company_name = product["nome_da_empresa"]
+                if hasattr(companies_dict, company_name):
+                    companies_dict[company_name] += 1
+                else:
+                    companies_dict[company_name] = 1
+            return reduce(lambda a, b: a if a > b else b, companies_dict)
+
+        oldest_fab_date = get_oldest_fab_date(dataReport)
+        closest_validity = get_closest_validity(dataReport)
+        company = get_company_with_more_products(dataReport)
 
         print(
-            f"""Data de fabricação mais antiga: {cls.get_oldest_fab_date(dataReport)}\n
-        Data de validade mais próxima: {cls.get_closest_validity(dataReport)}\n
-        Empresa com maior quantidade de produtos estocados: {cls.get_more_products(dataReport)}\n"""
+            f"""Data de fabricação mais antiga: {oldest_fab_date}\n
+Data de validade mais próxima: {closest_validity}\n
+Empresa com maior quantidade de produtos estocados: {company}\n"""
         )
-
-    def get_oldest_fab_date(products_list):
-        products_list.sort(
-            key=lambda product: transform_string_to_date(
-                product["data_de_fabricacao"]
-            )
-        )
-        return products_list[0]["data_de_fabricacao"]
-
-    def get_closest_validity(products_list):
-        today = datetime.today()
-        next_validity_dates = [
-            product["data_de_validade"]
-            for product in products_list
-            if today < transform_string_to_date(product["data_de_validade"])
-        ]
-        return min(next_validity_dates)
-
-    def get_more_products(products_list):
-        companies_dict = {}
-        for product in products_list:
-            company_name = product["nome_da_empresa"]
-            if hasattr(companies_dict, company_name):
-                companies_dict[company_name] += 1
-            else:
-                companies_dict[company_name] = 1
-        # retornar a empresa com mais produtos
