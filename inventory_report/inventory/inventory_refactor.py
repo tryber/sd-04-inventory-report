1faa9b7e9adf477b5fef05b93548bc55f5f0bc0e
Giorgia <giorgialeoniac@gmail.com>
2021-03-19 00:10:14 -0300

    Finalizando projeto com todos testes ok localmente

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,20 @@
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+from collections.abc import Iterable
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.data = []
+        self.importer = importer
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+    def import_data(self, file_path, type):
+        self.data += self.importer.import_data(file_path)
+        if type == "simples":
+            return SimpleReport.generate(self.data)
+        if type == "completo":
+            return CompleteReport.generate(self.data)
--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    try:
+        _argm1, file_path, mode_report = sys.argv
+    except ValueError:
+        sys.stderr.write('Verifique os argumentos\n')
+        return
+
+    if file_path.endswith(".csv"):
+        reports = CsvImporter
+    elif file_path.endswith(".json"):
+        reports = JsonImporter
+    else:
+        reports = XmlImporter
+
+    refactor = InventoryRefactor(reports)
+    result = refactor.import_data(file_path, mode_report)
+    print(result, end="")
