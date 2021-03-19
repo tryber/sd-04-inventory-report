607b4bc4e427d9f057db69ae299410d05143ee87
lucasfranca90 <luc.rangel90@gmail.com>
2021-03-19 19:28:54 -0300

    import requirements

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,15 @@
+import csv
+from inventory_report.importer.importer import Importer
+
+
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if not path.endswith(".csv"):
+            raise ValueError("Arquivo inválido")
+        with open(path) as csv_file:
+            data = []
+            csv_reader = csv.DictReader(csv_file)
+            for item in csv_reader:
+                data.append(item)
+            return data
---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+from inventory_report.importer.importer import Importer
+
+
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if not path.endswith(".xml"):
+            raise ValueError("Arquivo inválido")
+        with open(path) as path:
+            root = ET.parse(path).getroot()
+            data = []
+            for child in root:
+                obj = {}
+                for neto in child:
+                    obj[neto.tag] = neto.text
+                data.append(obj)
+            return data
