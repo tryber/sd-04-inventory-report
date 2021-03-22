d02dbcd449f768662b92caebe34a90c7ef53f174
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-22 19:27:38 -0300

    req 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from statistics import mode
+
+
+class SimpleReport:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def generate(self, stock):
+        today_day = datetime.today()
+        day_formated = f"{today_day.year}-0{today_day.month}-{today_day.day}"
+
+        fab_day = min(
+            [
+                datetime.strptime(
+                    data["data_de_fabricacao"], "%Y-%m-%d"
+                ).date()
+                for data in stock
+            ]
+        )  # cria a strind de data // https://www.programiz.com/python-programming/datetime/strptime
+
+        val_date = min(
+            [
+                datetime.strptime(data["data_de_validade"], "%Y-%m-%d").date()
+                for data in stock
+                if datetime.strptime(
+                    data["data_de_validade"], "%Y-%m-%d"
+                ).date()
+                > datetime.strptime(day_formated, "%Y-%m-%d").date()
+            ]
+        )
+
+        stock_data = mode([data["nome_da_empresa"] for data in stock])
+
+        brief = (
+            f"Data de fabricação mais antiga: {fab_day}\n"
+            f"Data de validade mais próxima: {val_date}\n"
+            f"Empresa com maior quantidade de produtos estocados: {stock_data}\n"
+        )
+
+        return brief
