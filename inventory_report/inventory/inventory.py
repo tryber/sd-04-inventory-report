530a59b30ab4c46787bf0f3fd203d0b4a44426a0
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2021-03-23 01:03:07 -0300

    req 3,4,5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -8,14 +8,21 @@ from inventory_report.importer.xml_importer import XmlImporter
 
 class Inventory:
     @classmethod
-    def simp_or_comp(self, data, type):
-        if type == 'simples':
+    def simp_or_comp(self, data, tipo):
+        if tipo == 'simples':
             return SimpleReport.generate(data)
         else:
             return CompleteReport.generate(data)
 
     @classmethod
-    def import_data(self, file_path, type):
+    def import_data(self, file_path, tipo):
         if file_path.endswith('.csv'):
             data_from_file = CsvImporter.import_data(file_path)
-            return self.simp_or_comp(data_from_file, type)
+            return self.simp_or_comp(data_from_file, tipo)
+        elif file_path.endswith('.json'):
+            data_from_file = JsonImporter.import_data(file_path)
+            return self.simp_or_comp(data_from_file, tipo)
+        elif file_path.endswith('.xml'):
+            data_from_file = XmlImporter.import_data(file_path)
+            return self.simp_or_comp(data_from_file, tipo)
+
